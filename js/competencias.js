0673365d8c67247fbbf993ff1100f2793dbcbe9c
Edwin Pinzon <edwinymartines@gmail.com>
2025-04-29 23:22:01 -0500

    correcion_RutaPrincipal

--- js/competencias.js
+++ js/competencias.js
@@ -42,6 +42,6 @@ $(document).ready(function() {
     localStorage.setItem('competencias', JSON.stringify(competencias));
     
     // Redirigir a la página principal
-    window.location.href ='index.html';
+    window.location.href =window.location.href = '/FrontAplicativoBoca/index.html';;
   });
 });
